bcc86ea2979c519a7d42ff354be23b56e76f011e
Vlad Kolotov <vkolotov@gmail.com>
2017-09-19 08:02:35 +1200

    Enabling travis CI Signed-off-by: Vlad Kolotov <vkolotov@gmail.com>

---
+++ .travis/deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+VERSION=$( echo "${TRAVIS_TAG##*-}" )
+if [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ ! -z "$TRAVIS_TAG" ]
+then
+    echo "on a tag -> set pom.xml <version> to $VERSION"
+    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$VERSION 1>/dev/null 2>/dev/null
+    mvn deploy -P sign,build-extras --settings .travis/settings.xml
+fi
